6e13a066ad47ec69d7ccfdc87be9c1a205a6fa57
racoutlaw <racoutlaw@yahoo.com>
2025-10-02 20:39:36 -0400

    Initial gardening guide setup

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import GardenPlantingApp from './GardenPlantingApp';
+
+function App() {
+  return <GardenPlantingApp />;
+}
+
+export default App;
---
+++ src/GardenPlantingApp.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const GardenPlantingApp = () => {
+  return (
+    <div>
+      <h1>Welcome to the Gardening Guide!</h1>
+      <p>Your personalized planting calendar will appear here.</p>
+    </div>
+  );
+};
+
+export default GardenPlantingApp;
